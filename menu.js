24df7c64e335e52422196f209d5d2ae78b5d75b1
Zara Bergh Wiklund <zara.wiklund@studerande.karlskogafhs.se>
2026-05-13 10:16:16 +0200

    Delete menu.js

--- menu.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById("menu-btn");
-const menu = document.getElementById("mobile-menu");
-
-btn.addEventListener("click", () => {
-  menu.classList.toggle("hidden");
-});
